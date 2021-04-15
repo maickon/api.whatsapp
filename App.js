2afd419b4da0b8d9b4b2aa6ccdb41bff5df026cb
Maickon <maickon4developers@gmail.com>
2021-04-15 16:23:14 -0300

    add a simple homa page

--- App.js
+++ App.js
@@ -29,4 +29,13 @@ app.get('/api/v1/send/:telephone', (req, res) =>  {
     });
 });
 
+app.get('/', (req, res) =>  {
+    htmlText = `
+        <h1>WhatsApp API</h1>
+        <p>Faça um chamada GET na rota /api/v1/send/:telephone?message=mensagem</p>
+        <p>Onde :telephone é o número de quem vai receber a mensagem e message é a mensagem a ser enviada.</p>
+    `;
+    res.send(htmlText);
+});
+
 app.listen(3333, () => console.log('Server running!'))
